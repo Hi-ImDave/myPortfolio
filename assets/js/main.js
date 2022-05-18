21e6797acbc2dea24818673e3b43fd9038e4750d
David Mott <d.s.mott0808@gmail.com>
2022-05-18 11:54:56 -0700

    create modal for project gifs

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,46 @@
 			noOpenerFade: true,
 			expandMode: (browser.mobile ? 'click' : 'hover')
 		});
+	
+	// MODAL - MY EDITS
+		// Get the modal
+		let modal = document.getElementById("myModal");
+					
+		// Get the image and insert it inside the modal - use its "alt" text as a caption
+		let img1 = document.getElementById("myImage1");
+		let img2 = document.getElementById("myImage2");
+		let img3 = document.getElementById("myImage3");
+		let img4 = document.getElementById("myImage4");
+		let modalImg = document.getElementById("img01");
+		let captionText = document.getElementById("caption");
+		img1.onclick = function(){
+		  modal.style.display = "block";
+		  modalImg.src = this.src;
+		  captionText.innerHTML = this.alt;
+		}
+		img2.onclick = function(){
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		  }
+		  img3.onclick = function(){
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		  }
+		  img4.onclick = function(){
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		  }
+		
+		// Get the <span> element that closes the modal
+		let span = document.getElementsByClassName("close")[0];
+		
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		  modal.style.display = "none";
+		} 
 
 	// Nav Panel.
 
